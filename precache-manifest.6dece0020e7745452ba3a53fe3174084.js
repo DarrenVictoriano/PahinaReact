50cfa88dd801d71919a98226efde4c484682cb3a
Darren Victoriano <darren.victoriano@gmail.com>
2020-10-13 19:32:09 -0700

    Updates

---
+++ precache-manifest.6dece0020e7745452ba3a53fe3174084.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53f154f02dc9210c200276e76c2dca1",
+    "url": "/PahinaReact/index.html"
+  },
+  {
+    "revision": "7cac1c8e22ea2d1d04f0",
+    "url": "/PahinaReact/static/css/2.06de684b.chunk.css"
+  },
+  {
+    "revision": "c8033a6d5f9ecb3370d2",
+    "url": "/PahinaReact/static/css/main.6b63f049.chunk.css"
+  },
+  {
+    "revision": "7cac1c8e22ea2d1d04f0",
+    "url": "/PahinaReact/static/js/2.a899cf9d.chunk.js"
+  },
+  {
+    "revision": "d8a5bd68479a9874a45aca3297a21263",
+    "url": "/PahinaReact/static/js/2.a899cf9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8033a6d5f9ecb3370d2",
+    "url": "/PahinaReact/static/js/main.565f9c77.chunk.js"
+  },
+  {
+    "revision": "14fae390857292ed31ee",
+    "url": "/PahinaReact/static/js/runtime-main.fc4d717b.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/PahinaReact/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/PahinaReact/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/PahinaReact/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/PahinaReact/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/PahinaReact/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/PahinaReact/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/PahinaReact/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/PahinaReact/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/PahinaReact/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/PahinaReact/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/PahinaReact/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/PahinaReact/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/PahinaReact/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/PahinaReact/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/PahinaReact/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
